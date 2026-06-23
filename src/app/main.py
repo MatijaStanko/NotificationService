4642c602e1bb060a67130df6a39649cfc9403d99
MatijaStanko <matija.stankovic@ibis-solutions.rs>
2026-06-23 13:39:34 +0200

    Set up PostgreSQL db with SQLModel tables

--- src/app/main.py
+++ src/app/main.py
@@ -1,12 +1,19 @@
 from fastapi import FastAPI
 
+from app import models
 from app.config import settings
+from app.database import create_db_and_tables
 from routers import notifications
 app = FastAPI(
     title = settings.app_name,
     version = settings.app_version,
     debug = settings.debug
 )
+
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
 
 @app.get("/health")
 def health_check():
---
+++ src/app/models.py
@@ -0,0 +1,51 @@
+from datetime import datetime
+from typing import Optional
+
+from sqlalchemy import Column
+from sqlmodel import SQLModel, Field
+from sqlalchemy.dialects.postgresql import JSONB
+
+class NotificationTypes(SQLModel, table=True):
+    __tablename__ = "notification_types"
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    code : str = Field(index=True, unique=True)
+    is_active : bool = True
+
+class NotificationTemplate(SQLModel, table=True):
+    __tablename__ = "notification_templates"
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    notification_type_id : int = Field(foreign_key="notification_types.id")
+    channel_id : int = Field(foreign_key="channel_config.id", index=True)
+    subject_template: Optional[str] = None
+    body_template: str
+    required_variables: dict = Field(default_factory=dict, sa_column=Column(JSONB))
+    is_active: bool = True
+
+class ChannelConfig(SQLModel, table=True):
+    __tablename__ = "channel_config"
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    channel: str = Field(index=True, unique=True)
+    provider: str
+    config : dict = Field(default_factory=dict, sa_column=Column(JSONB))
+    is_active: bool = True
+
+class NotificationRequests(SQLModel, table=True):
+    __tablename__ = "notification_requests"
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    source_service: Optional[str] = None
+    notification_type_id: int = Field(foreign_key="notification_types.id")
+    template_id: int = Field(foreign_key="notification_templates.id")
+    channel: str = Field(index=True)
+    recipient: str = Field(index=True)
+    template_data: dict = Field(default_factory=dict, sa_column=Column(JSONB))
+    rendered_subject: Optional[str] = None
+    rendered_body: Optional[str] = None
+    status: str = Field(index=True, default='pending')
+    error_msg: Optional[str] = None
+    created_at: datetime = Field(default_factory=datetime.utcnow)
+    sent_at: Optional[datetime] = None
+    updated_at: datetime = Field(default_factory=datetime.utcnow)
